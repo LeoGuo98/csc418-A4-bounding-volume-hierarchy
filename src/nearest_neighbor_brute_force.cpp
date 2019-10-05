05ef3f30c9372e48bf533b9aa102265ca1c0db03
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-05 15:38:07 -0400

    Copy over code from A2 - Triangle Intersection
    - Copied Plane and Triangle intersection from A2 -> still need to test
            if this works. I am typecasting Eigen::Vector3d to Eigen::RowVector3d
            and IDK if this is ok

--- src/nearest_neighbor_brute_force.cpp
+++ src/nearest_neighbor_brute_force.cpp
@@ -1,15 +1,22 @@
 #include "nearest_neighbor_brute_force.h"
 #include <limits>// std::numeric_limits<double>::infinity();
 
+// double squared_distance(const int &x1, const int &x2){
+//   // actually, 3D
+//   return sqrt((pow(x1, 2) + pow(x2, 2)));
+// }
 void nearest_neighbor_brute_force(
   const Eigen::MatrixXd & points,
   const Eigen::RowVector3d & query,
   int & I,
   double & sqrD)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
   I = -1;
   sqrD = 0;
-  ////////////////////////////////////////////////////////////////////////////
+
+  // double min_dist = std::numeric_limits<double>::infinity();
+  // int points_size = points.size();
+  // for (int i = 0; i < points_size; i++){
+  //   auto point = points[i];
+  // }
 }
--- src/ray_intersect_triangle.cpp
+++ src/ray_intersect_triangle.cpp
@@ -1,5 +1,30 @@
 #include "ray_intersect_triangle.h"
 
+bool intersect_plane(
+  const Ray & ray, const double min_t, const double max_t, const Eigen::RowVector3d &plane_point, const Eigen::RowVector3d &plane_normal, double & t)
+{
+    // From A2
+  Eigen::RowVector3d e = ray.origin, d = ray.direction;
+
+    double numerator = (plane_point - e).dot(plane_normal);
+    double denumerator = d.dot(plane_normal);
+    if (0 <= denumerator && denumerator<= 1e-6){
+        if (0 <= numerator && numerator <= 1e-6){
+            // ray lines in the plane
+            // not sure if this is the right way to handle this or not
+            t = min_t;
+            return true;
+        }else // they never intersect
+            return false;
+    }
+    // They intersect, check if >= min_t
+    t = numerator / denumerator;
+    if (t >= min_t && t <= max_t){
+        return true;
+    }
+    return false;
+}
+
 bool ray_intersect_triangle(
   const Ray & ray,
   const Eigen::RowVector3d & A,
@@ -9,10 +34,30 @@ bool ray_intersect_triangle(
   const double max_t,
   double & t)
 {
-  ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
   t = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
-}
 
+  Eigen::RowVector3d q = B - A;
+  Eigen::RowVector3d p = C - A;
+  Eigen::RowVector3d tri_n = (q).cross(p).normalized();
+  if (intersect_plane(ray, min_t, max_t, A, tri_n, t)){
+      // we now need to check and see if the the ray is inside the
+      // triangle
+      Eigen::RowVector3d point = ray.origin + t * ray.direction;
+      Eigen::RowVector3d edge0 = B - A;
+      Eigen::RowVector3d edge1 = C - B;
+      Eigen::RowVector3d edge2 = A - C;
+
+      Eigen::RowVector3d x0 = point - A;
+      Eigen::RowVector3d x1 = point - B;
+      Eigen::RowVector3d x2 = point - C;
+
+      if (tri_n.dot(edge0.cross(x0)) > 0 &&
+          tri_n.dot(edge1.cross(x1)) > 0 &&
+          tri_n.dot(edge2.cross(x2)) > 0){
+          return true;
+      }
+
+  }
+  return false;
+}
