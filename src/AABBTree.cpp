68660d84f5eb3f47528bc0ba3d361a1089f314da
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-06 17:11:49 -0400

    Commit before pulling from whatever

--- src/AABBTree.cpp
+++ src/AABBTree.cpp
@@ -1,15 +1,16 @@
 #include "AABBTree.h"
 #include "insert_box_into_box.h"
+#include "ray_intersect_box.h"
 
 AABBTree::AABBTree(
   const std::vector<std::shared_ptr<Object> > & objects,
-  int a_depth): 
-  depth(std::move(a_depth)), 
+  int a_depth):
+  depth(std::move(a_depth)),
   num_leaves(objects.size())
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  int objects_size = objects.size();
+  for (int i = 0; i < objects_size; i++){
+  }
 }
 
 bool AABBTree::ray_intersect(
@@ -17,11 +18,8 @@ bool AABBTree::ray_intersect(
   const double min_t,
   const double max_t,
   double & t,
-  std::shared_ptr<Object> & descendant) const 
+  std::shared_ptr<Object> & descendant) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
   t = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  return(this->ray_intersect(ray, min_t, max_t, t, descendant));
 }
