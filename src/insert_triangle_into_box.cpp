5a21579fd4eef09b9fd2357f9c8a64159252d976
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-06 16:07:13 -0400

    I think done insert triangle into box

--- src/insert_triangle_into_box.cpp
+++ src/insert_triangle_into_box.cpp
@@ -6,9 +6,31 @@ void insert_triangle_into_box(
   const Eigen::RowVector3d & c,
   BoundingBox & B)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  auto min_vertex_x = a(0) < b(0) && a(0) < c(0)? a(0): \
+                      b(0) < c(0)? b(0): c(0);
+  auto min_vertex_y = a(1) < b(1) && a(1) < c(1)? a(1): \
+                      b(1) < c(1)? b(1): c(1);
+  auto min_vertex_z = a(2) < b(2) && a(2) < c(2)? a(2): \
+                      b(2) < c(2)? b(2): c(2);
+  auto max_vertex_x = a(0) > b(0) && a(0) > c(0)? a(0): \
+                      b(0) > c(0)? b(0): c(0);
+  auto max_vertex_y = a(1) > b(1) && a(1) > c(1)? a(1): \
+                      b(1) > c(1)? b(1): c(1);
+  auto max_vertex_z = a(2) > b(2) && a(2) > c(2)? a(2): \
+                      b(2) > c(2)? b(2): c(2);
+
+  B.min_corner(0) = B.min_corner(0) < min_vertex_x? B.min_corner(0): \
+                    min_vertex_x;
+  B.min_corner(1) = B.min_corner(0) < min_vertex_y? B.min_corner(1): \
+                    min_vertex_y;
+  B.min_corner(2) = B.min_corner(0) < min_vertex_z? B.min_corner(2): \
+                    min_vertex_z;
+  B.max_corner(0) = B.max_corner(0) < min_vertex_x? B.min_corner(0): \
+                    min_vertex_x;
+  B.max_corner(1) = B.max_corner(1) < min_vertex_y? B.min_corner(1): \
+                    min_vertex_y;
+  B.max_corner(2) = B.max_corner(2) < min_vertex_z? B.min_corner(2): \
+                    min_vertex_z;
 }
 
 
