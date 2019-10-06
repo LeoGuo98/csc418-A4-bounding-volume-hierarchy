3cd9066ef9418b691db2d3af142a5b8cbae4a172
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-06 19:03:03 -0400

    Done AABBTree I think. Need testing

--- src/AABBTree.cpp
+++ src/AABBTree.cpp
@@ -8,7 +8,48 @@ AABBTree::AABBTree(
   num_leaves(objects.size())
 {
   int objects_size = objects.size();
-  for (int i = 0; i < objects_size; i++){
+  if (objects_size > 1){
+    for (int i = 0; i < objects_size; i++){
+      insert_box_into_box(objects[i]->box, this->box);
+    }
+
+    auto x_axis_len = this->box.max_corner(0) - this->box.min_corner(0);
+    auto x_axis_midpoint = x_axis_len / 2.0;
+    auto y_axis_len = this->box.max_corner(1) - this->box.min_corner(1);
+    auto y_axis_midpoint = y_axis_len / 2.0;
+    auto z_axis_len = this->box.max_corner(2) - this->box.min_corner(2);
+    auto z_axis_midpoint = z_axis_len / 2.0;
+    std::vector<std::shared_ptr<Object>> left;
+    std::vector<std::shared_ptr<Object>> right;
+    std::shared_ptr<Object> object;
+    for (int i = 0; i < objects_size; i++){
+      object = objects[i];
+      if(x_axis_len > y_axis_len && x_axis_len > z_axis_len){
+        if (object->box.center()(0) < x_axis_midpoint)
+          left.push_back(object);
+        else
+          right.push_back(object);
+      }
+      else if (y_axis_len > z_axis_len){
+        if (object->box.center()(1) < y_axis_midpoint)
+          left.push_back(object);
+        else
+          right.push_back(object);
+      }
+      else{
+        if (object->box.center()(2) < z_axis_midpoint)
+          left.push_back(object);
+        else
+          right.push_back(object);
+      }
+    }
+    if (left.size() == 1)
+      this->left = left[0];
+    else
+      *this->left = AABBTree(left, a_depth + 1);
+    if (right.size() == 1)
+      this->right = right[0];
+    else
+      *this->right = AABBTree(right, a_depth + 1);
   }
-}
 }
