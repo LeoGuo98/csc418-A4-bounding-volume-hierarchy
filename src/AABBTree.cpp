a2426db6ceee48b91e2d6c218e17a037df805dcd
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-07 16:25:53 -0400

    Done AABBTree: issue was inf recur

--- src/AABBTree.cpp
+++ src/AABBTree.cpp
@@ -20,15 +20,21 @@ AABBTree::AABBTree(
       insert_box_into_box(objects[i]->box, this->box);
     }
 
-    auto x_axis_len = this->box.max_corner.normalized()(0) - \
-                      this->box.min_corner.normalized()(0);
-    auto x_axis_midpoint = x_axis_len / 2.0;
-    auto y_axis_len = this->box.max_corner.normalized()(1) - \
-                      this->box.min_corner.normalized()(1);
-    auto y_axis_midpoint = y_axis_len / 2.0;
-    auto z_axis_len = this->box.max_corner.normalized()(2) - \
-                      this->box.min_corner.normalized()(2);
-    auto z_axis_midpoint = z_axis_len / 2.0;
+    auto x_axis_len = abs(this->box.max_corner.x() - \
+                          this->box.min_corner.x());
+    auto x_axis_midpoint = (this->box.max_corner.x() + \
+                            this->box.min_corner.x()) / 2.0;
+
+    auto y_axis_len = abs(this->box.max_corner.y() - \
+                          this->box.min_corner.y());
+    auto y_axis_midpoint = (this->box.max_corner.y() + \
+                            this->box.min_corner.y()) / 2.0;
+
+    auto z_axis_len = abs(this->box.max_corner.z() - \
+                          this->box.min_corner.z());
+    auto z_axis_midpoint = (this->box.max_corner.z() + \
+                            this->box.min_corner.z()) / 2.0;
+
     assert(!isinf(x_axis_len) && "X axis inf");
     assert(!isinf(y_axis_len) && "Y axis inf");
     assert(!isinf(z_axis_len) && "Z axis inf");
@@ -39,32 +45,51 @@ AABBTree::AABBTree(
     for (int i = 0; i < objects_size; i++){
       object = objects[i];
       if(x_axis_len > y_axis_len && x_axis_len > z_axis_len){
-        if (object->box.center()(0) < x_axis_midpoint)
+        if (object->box.center().x() < x_axis_midpoint)
           left.push_back(object);
         else
           right.push_back(object);
       }
       else if (y_axis_len > z_axis_len){
-        if (object->box.center()(1) < y_axis_midpoint)
+        if (object->box.center().y() < y_axis_midpoint)
           left.push_back(object);
         else
           right.push_back(object);
       }
       else{
-        if (object->box.center()(2) < z_axis_midpoint)
+        if (object->box.center().z() < z_axis_midpoint)
           left.push_back(object);
         else
           right.push_back(object);
       }
     }
-    if (left.size() == 1)
-      this->left = left[0];
-    else if (left.size() > 1)
-      *this->left = AABBTree(left, a_depth + 1);
 
-    if (right.size() == 1)
-      this->right = right[0];
-    else if (right.size() > 1)
-      *this->right = AABBTree(right, a_depth + 1);
+    if (left.size() == objects.size()){
+      std::vector<std::shared_ptr<Object>> new_left;
+      std::vector<std::shared_ptr<Object>> new_right;
+      for(int i = 0; i < left.size() / 2; i++)
+        new_left.push_back(left[i]);
+      for(int i = left.size() / 2; i < left.size(); i++)
+        new_right.push_back(left[i]);
+    }
+    else if (right.size() == objects.size()){
+      std::vector<std::shared_ptr<Object>> new_left;
+      std::vector<std::shared_ptr<Object>> new_right;
+      for(int i = 0; i < right.size() / 2; i++)
+        new_left.push_back(right[i]);
+      for(int i = right.size() / 2; i < right.size(); i++)
+        new_right.push_back(right[i]);
+    }
+    else{
+      if (left.size() == 1)
+        this->left = left[0];
+      if (left.size() > 1)
+        this->left = std::make_shared<AABBTree>(left, a_depth + 1);
+
+      if (right.size() == 1)
+        this->right = right[0];
+      if (right.size() > 1)
+        this->right = std::make_shared<AABBTree>(right, a_depth + 1);
+    }
   }
 }
