f87929749b0f846be96883c95dd223f77db31ac9
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-08 11:48:22 -0400

    WIP T intersect T. Need testing

--- src/triangle_triangle_intersection.cpp
+++ src/triangle_triangle_intersection.cpp
@@ -1,4 +1,6 @@
 #include "triangle_triangle_intersection.h"
+#include "Ray.h"
+#include "ray_intersect_triangle.h"
 
 bool triangle_triangle_intersection(
   const Eigen::RowVector3d & A0,
@@ -8,8 +10,27 @@ bool triangle_triangle_intersection(
   const Eigen::RowVector3d & B1,
   const Eigen::RowVector3d & B2)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
+  // Simply, construct a 'ray' for each edge of the second triangle
+  // and use the ray intersect triangle function, where min/max are defined
+  // by the line that edge
+  double min_t = -1, max_t = 1, t;
+  bool intersect = false;
+  Ray ray(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 0));
+
+  ray.origin = B0;
+  ray.direction = B0 - B1;
+  if(ray_intersect_triangle(ray, A0, A1, A2, min_t, max_t, t))
+    return true;
+
+  ray.origin = B1;
+  ray.direction = B1 - B2;
+  if(ray_intersect_triangle(ray, A0, A1, A2, min_t, max_t, t))
+    return true;
+
+  ray.origin = B0;
+  ray.direction = B0 - B2;
+  if(ray_intersect_triangle(ray, A0, A1, A2, min_t, max_t, t))
+    return true;
+
   return false; 
-  ////////////////////////////////////////////////////////////////////////////
 }
