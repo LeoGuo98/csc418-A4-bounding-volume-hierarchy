616d279e7295690e51dc9f77208c0d4cdf7a2589
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-07 22:13:29 -0400

    More clean vertice get

--- src/ray_intersect_triangle_mesh_brute_force.cpp
+++ src/ray_intersect_triangle_mesh_brute_force.cpp
@@ -22,11 +22,14 @@ bool ray_intersect_triangle_mesh_brute_force(
 
   Eigen::RowVector3d a, b, c; // vertices of triangle
   for (int i = 0; i < f_rows; i++){
-    for (int j = 0; j < 3; j++){
-      a(j) = V(F(i, 0), j);
-      b(j) = V(F(i, 1), j);
-      c(j) = V(F(i, 2), j);
-    }
+    // for (int j = 0; j < 3; j++){
+    //   a(j) = V(F(i, 0), j);
+    //   b(j) = V(F(i, 1), j);
+    //   c(j) = V(F(i, 2), j);
+    // }
+    a = V.row(F(i, 0));
+    b = V.row(F(i, 1));
+    c = V.row(F(i, 2));
     if (ray_intersect_triangle(ray, a, b, c, min_t, max_t, hit_t)){
       if (hit_t < best_t){
         best_t = hit_t;
