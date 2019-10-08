0f0fa38e3b6d34d0234e73736eaa56346da72fa1
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-08 12:30:13 -0400

    WIP Tree find all intersections

--- src/find_all_intersecting_pairs_using_AABBTrees.cpp
+++ src/find_all_intersecting_pairs_using_AABBTrees.cpp
@@ -1,5 +1,7 @@
 #include "find_all_intersecting_pairs_using_AABBTrees.h"
 #include "box_box_intersect.h"
+#include "MeshTriangle.h"
+#include "AABBTree.h"
 // Hint: use a list as a queue
 #include <list> 
 
@@ -9,7 +11,48 @@ void find_all_intersecting_pairs_using_AABBTrees(
   std::vector<std::pair<std::shared_ptr<Object>,std::shared_ptr<Object> > > & 
     leaf_pairs)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  std::list<std::pair<std::shared_ptr<Object>, std::shared_ptr<Object>>> queue;
+  std::pair<std::shared_ptr<Object>, std::shared_ptr<Object>> node_pair;
+  std::shared_ptr<Object> node_a, node_b;
+  std::shared_ptr<MeshTriangle> mesh_t_a, mesh_t_b;
+  std::shared_ptr<AABBTree> tree_a, tree_b;
+  if (box_box_intersect(rootA->box, rootB->box))
+    queue.push_back(std::make_pair(rootA, rootB));
+  while (!queue.empty()){
+    node_pair = queue.front(); 
+    node_a = node_pair.first;
+    node_b = node_pair.second;
+    
+    mesh_t_a = std::dynamic_pointer_cast<MeshTriangle>(node_a);
+    mesh_t_b = std::dynamic_pointer_cast<MeshTriangle>(node_b);
+
+    if (mesh_t_a && mesh_t_b)
+      leaf_pairs.push_back(std::make_pair(node_a, node_b));
+    else if (mesh_t_a){
+      tree_b = std::dynamic_pointer_cast<AABBTree>(node_b);
+      if (box_box_intersect(node_a->box, tree_b->left->box))
+        queue.push_back(std::make_pair(node_a, tree_b->left));
+      if (box_box_intersect(node_a->box, tree_b->right->box))
+        queue.push_back(std::make_pair(node_a, tree_b->right));
+    }
+    else if (mesh_t_b){
+      tree_a = std::dynamic_pointer_cast<AABBTree>(node_a);
+      if (box_box_intersect(node_b->box, tree_a->left->box))
+        queue.push_back(std::make_pair(node_b, tree_a->left));
+      if (box_box_intersect(node_b->box, tree_a->right->box))
+        queue.push_back(std::make_pair(node_b, tree_a->right));
+    }
+    else{
+      tree_a = std::dynamic_pointer_cast<AABBTree>(node_a);
+      tree_b = std::dynamic_pointer_cast<AABBTree>(node_b);
+      if (box_box_intersect(tree_a->left->box, tree_b->left->box))
+        queue.push_back(std::make_pair(tree_a->left, tree_b->left));
+      if (box_box_intersect(tree_a->left->box, tree_b->right->box))
+        queue.push_back(std::make_pair(tree_a->left, tree_b->right));
+      if (box_box_intersect(tree_a->right->box, tree_b->left->box))
+        queue.push_back(std::make_pair(tree_a->right, tree_b->left));
+      if (box_box_intersect(tree_a->right->box, tree_b->right->box))
+        queue.push_back(std::make_pair(tree_a->right, tree_b->right));
+    }
+  }
 }
